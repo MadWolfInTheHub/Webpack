92d35b6d06ae47b2d2b922a55c75b1c97c2d0c2e
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-20 16:04:22 +0800

    test

--- lesson4/task1/polyfills/index.js
+++ lesson4/task1/polyfills/index.js
@@ -1,5 +1,5 @@
 export const printProfile = profileData => {
     console.log('implementation for printProfile');
-    const { name, company } = profileData;
-    console.log(`${name} from ${company}`);
+    const { name, from } = profileData;
+    console.log(`${name} from ${from}`);
 };
