740f29833173d35b7a542a0fc88bbf2cf56796ec
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-03-24 18:16:00 +0800

    test

--- lesson4/task1/profile/gateway.js
+++ lesson4/task1/profile/gateway.js
@@ -2,12 +2,12 @@
 // экспортируйте fetchUser как export по умолчанию
 
 async function fetchUser(userId) {
-    console.log('implementation for fetchUser');
-    const response = await fetch(`https://api.github.com/users/${userId}`);
-    if (!response.ok) {
-      throw new Error('Failed to get user data');
-    }
-    return await response.json();
+  console.log('implementation for fetchUser');
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  if (!response.ok) {
+    throw new Error('Failed to get user data');
+  }
+  return await response.json();
 }
 
 export default fetchUser;
