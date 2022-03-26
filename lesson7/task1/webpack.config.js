efd714a305b00e9f5b7af40d9e19b22eed09fafd
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-03-26 18:38:03 +0800

    Todo List webpack plugins

--- lesson7/task1/webpack.config.js
+++ lesson7/task1/webpack.config.js
@@ -1,5 +1,5 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
